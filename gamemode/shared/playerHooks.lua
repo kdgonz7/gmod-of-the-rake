194bfe6fc044689f2cc7a92e3d74c2ad419961a8
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-18 22:43:33 -0400

    Add a Rake Win condition & exchange random pickups for loadouts and supply drops

--- gamemode/shared/playerHooks.lua
+++ gamemode/shared/playerHooks.lua
@@ -29,8 +29,20 @@ concommand.Add("rake_StartGame", function(ply, cmd, args, str)
 end)
 
 concommand.Add("rake_EndMatch", function(ply, cmd, args, str)
-	if not ply:IsSuperAdmin() or adminManager:PlayerIsAdmin(ply:SteamID64()) then return end
+	if not ply:IsSuperAdmin() or not adminManager:PlayerIsAdmin(ply:SteamID64()) then return end
 	roundManage:EndRound(REASON_ROUNDCOMPLETE)
+end)
+
+hook.Add("OnNPCKilled", "RakeNPCKilled", function(npc, attacker, inflictor)
+	if npc == roundManage.RakeEntity then
+		npc:BecomeRagdoll()
+		for _, pl in pairs(player.GetAll()) do
+			pl:Lock()
+		end
+
+		roundManage.LastRakeKiller = attacker
+		roundManage:EndRound(REASON_WINNER)
+	end
 end)
 
 hook.Add("PlayerSetModel", "RakePlayerModel", function(ply) ply:SetModel("models/player/combine_soldier.mdl") end)
@@ -38,10 +50,12 @@ hook.Add("PlayerSetModel", "RakePlayerModel", function(ply) ply:SetModel("models
 hook.Add("Initialize", "RakeDRGBaseSettings", function()
 	RunConsoleCommand("drgbase_ai_radius", 797)
 	RunConsoleCommand("drgbase_ai_patrol", 1)
-	RunConsoleCommand("drgbase_ai_sight", 0)
+	RunConsoleCommand("drgbase_ai_sight", 1)
 	RunConsoleCommand("drgbase_ai_hearing", 1)
 	RunConsoleCommand("mgbase_sv_customization", 0)
 	RunConsoleCommand("fpsfog_active", 0)
+
+	roundManage:ModifyStatus(IN_LOBBY)
 end)
 
 hook.Add("PlayerInitialSpawn", "AdminCheckAndEtc", function(ply)
@@ -55,6 +69,8 @@ hook.Add("PlayerSpawn", "RakeSpawnPlayer", function(ply)
 	if not IsValid(ply) then return end
 	if not roundManager:FindPlayer(ply) then roundManager:AddPlayerObjectToCache(ply) end
 
+	ply.WeaponClass = "Assault"	// assault gives a M4A1, and a Renetti
+
 	ply:RemoveAllAmmo()
 
 	if roundManage:GetRoundStatus() == IN_LOBBY then
@@ -63,8 +79,6 @@ hook.Add("PlayerSpawn", "RakeSpawnPlayer", function(ply)
 		ply:GiveAmmo(550, "Pistol", true)
 		ply:GodEnable()
 	else
-		// This is most likely called when we're in the match 
-		// and somebody joins, we'll handle this
 	end
 end)
 
@@ -98,5 +112,21 @@ hook.Add("PlayerDeath", "RakeRespawn", function(ply, inflictor, attacker)
 	end
 end)
 
+/*
+only allow one type of each weapon to be picked up
+*/
+hook.Add("PlayerCanPickupWeapon", "RakeAmmoCheck", function (ply, wep)
+	local plyWeapons = ply:GetWeapons()
+
+	for _, v in ipairs(plyWeapons) do
+		if (v:GetPrimaryAmmoType() == wep:GetPrimaryAmmoType()) then
+			ply:DropWeapon(v, nil, Vector(0, 5, 1))
+		end
+	end
+
+	return true
+end)
+
 hook.Add("PlayerNoClip", "NoNoClip", function(ply, des) return false end)
+
 print("loaded player hooks")
