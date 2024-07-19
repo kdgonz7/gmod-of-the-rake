9901f591c759939dec294c421ac35742bbd64da2
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-18 23:48:53 -0400

    Add a startgame menu (unfinished)

--- gamemode/cl/startGameHUD.lua
+++ gamemode/cl/startGameHUD.lua
@@ -1,24 +1,41 @@
-hook.Add("InitPostEntity", "StartGameHUD", function()
-	local frame = vgui.Create("DFrame")
-	frame:SetSize(1000, 750)
-	frame:Center()
-	frame:MakePopup()
-	frame:SetVisible(true)
-	local helloText = vgui.Create("DLabel", frame)
-	helloText:SetPos(10, 55)
-	helloText:SetSize(100, 500)
-	helloText:SetWrap(true)
-	helloText:SetText("Hello, " .. LocalPlayer():Nick() .. ", this is the RAKE! To start the game, you can type rake_StartGame in the console (as a superadmin), otherwise there should be a button if you don't close this panel out.")
+net.Receive("startgamehud", function(len)
+	if GetConVar("rake_GameState"):GetString() == "match" then return end
+		if not frame then
+			local frame = vgui.Create("DFrame")
 
-	if LocalPlayer():IsSuperAdmin() then
-		local startButton = vgui.Create("DButton", frame)
-		startButton:SetPos(10, 40)
-		startButton:SetSize(200, 45)
-		startButton:SetText("Start Current Game")
-		startButton.DoClick = function()
-			RunConsoleCommand("rake_StartGame")
+			frame:SetSize(500, 500	)
+			frame:SetTitle("The RAKE Menu")
+
+			frame:SetVisible(true)
+			frame:SetDraggable(false)
+			frame:ShowCloseButton(true)
 			frame:SetDeleteOnClose(true)
-			frame:Close()
-		end
+
+			frame:Center()
+			frame:MakePopup()
+
+			local startLabel = vgui.Create( "DLabel", frame )
+			startLabel:SetPos( 210, 100 )
+			startLabel:SetSize( 150, 30 )
+			startLabel:SetText( "Click start to start the game" )
+
+			local Panel = vgui.Create( "DPanel", frame )
+			Panel:SetPos( 10, 100 )
+			Panel:SetSize( 150, 150 )
+
+			local icon = vgui.Create( "DModelPanel", Panel )
+			icon:SetSize( 150, 150 )
+			icon:SetModel( LocalPlayer():GetModel() )
+
+			local Button = vgui.Create( "DButton", frame )
+			Button:SetPos( 210, 150 )
+			Button:SetSize( 150, 30 )
+			Button:SetText( "Start Game" )
+			Button.DoClick = function()
+				net.Start("startgame")
+				net.SendToServer()
+			end
+
+
 	end
 end)
