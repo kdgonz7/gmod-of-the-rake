bb64a23d96997a3ee07f93c296f4df3e62de96c8
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-20 01:24:52 -0400

    Close Window when game is started

--- gamemode/cl/startGameHUD.lua
+++ gamemode/cl/startGameHUD.lua
@@ -2,9 +2,11 @@ net.Receive("startgamehud", function(len)
 	if GetConVar("rake_GameState"):GetString() == "match" then return end
 		if not frame then
 			local frame = vgui.Create("DFrame")
+			local w = ScrW() * 0.5
+			local h = ScrH() * 0.5
 
-			frame:SetSize(500, 500	)
-			frame:SetTitle("The RAKE Menu")
+			frame:SetSize(w, h)
+			frame:SetTitle("The Rake by Kai D.")
 
 			frame:SetVisible(true)
 			frame:SetDraggable(false)
@@ -23,16 +25,13 @@ net.Receive("startgamehud", function(len)
 			Panel:SetPos( 10, 100 )
 			Panel:SetSize( 150, 150 )
 
-			local icon = vgui.Create( "DModelPanel", Panel )
-			icon:SetSize( 150, 150 )
-			icon:SetModel( LocalPlayer():GetModel() )
-
 			local Button = vgui.Create( "DButton", frame )
 			Button:SetPos( 210, 150 )
 			Button:SetSize( 150, 30 )
 			Button:SetText( "Start Game" )
 			Button.DoClick = function()
 				RunConsoleCommand("rake_StartGame")
+				frame:Close()
 			end
 
 
