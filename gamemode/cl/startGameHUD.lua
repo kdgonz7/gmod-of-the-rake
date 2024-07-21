3cf2053d70c54037dbf51352232f6f40f7c0f5c5
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-21 18:02:31 -0400

    Add a better looking panel

--- gamemode/cl/startGameHUD.lua
+++ gamemode/cl/startGameHUD.lua
@@ -18,21 +18,34 @@ hook.Add("Initialize", "RakeAddFonts", function()
 	})
 end)
 
-local function DrawMultilineText(text, font, x, y, color, align)
-	surface.SetFont(font)
+local function DrawMultilineText(parent, text, font, x, y, color)
+	text = string.Explode("\n", text)
 
-	local w, h = surface.GetTextSize(text)
-	local y = y - h
+	local offset = 0
 
-	draw.DrawText(text, font, x, y, color, align)
+	for _, v in ipairs(text) do
+		local l = vgui.Create("DLabel", parent)
+
+		l:SetFont(font)
+		l:SetPos(x, y + offset)
+		l:SetColor(color)
+		l:SetText(v)
+		l:SetWidth(1000)
+		l:SizeToContents()  -- Adjust the size to fit the text
+
+
+		offset = offset + l:GetTall()
+	end
 end
+
+
 
 net.Receive("startgamehud", function(len)
 	if GetConVar("rake_GameState"):GetString() == "match" then return end
 		if not frame then
 			local frame = vgui.Create("DFrame")
-			local width = ScrW() * 0.5
-			local height = ScrH() * 0.5
+			local width = ScrW() * 0.8
+			local height = ScrH() * 0.8
 
 
 			frame:SetSize(width, height)
@@ -40,11 +53,11 @@ net.Receive("startgamehud", function(len)
 
 			frame:SetVisible(true)
 			frame:SetDraggable(false)
-			frame:ShowCloseButton(true)
+			frame:ShowCloseButton(false)
 			frame:SetDeleteOnClose(true)
 
 			frame.Paint = function(self, w, h)
-				draw.RoundedBox(5, 0, 0, w, h, Color(68, 68, 68))
+				draw.RoundedBox(0, 0, 0, w, h, Color(40, 40, 40, 200))
 			end
 
 			frame:Center()
@@ -57,13 +70,14 @@ net.Receive("startgamehud", function(len)
 				draw.RoundedBox(5, 0, 0, w, h, Color(68, 68, 68, 0))
 			end
 
-			DrawMultilineText("Welcome to the rake.", "MainUIFont", 10, 10, Color(255, 255, 255), 1)
+			local ext = vgui.Create( "DPanel", frame )
+			ext:Dock( LEFT)
+			ext:DockMargin(0, 0, 0, 0)
+			ext.Paint = function(self, w, h)
+				draw.RoundedBox(5, 0, 0, w, h, Color(77, 77, 77))
+			end
 
-			local startLabel = vgui.Create( "DLabel", frame )
-			startLabel:SetText( "Click start to start the game" )
-			startLabel:SetFont("MainUIFont")
-			startLabel:SetPos(200, 100)
-			startLabel:SetSize(500, 50)
+			DrawMultilineText(frame, "Welcome to the rake. A gamemode about\nsurvival and killing an anomaly\nknown as the rake.\n\nTo survive you must have teamwork,\nand a determination to survive.\n\nHow to play: Press `Start Game`. You start off with the Assault class, but can upgrade as your XP improves.", "MainUIFont", 100, 50, Color(255, 255, 255))
 
 			local Button = vgui.Create( "DButton", frame )
 			Button:SetPos( 210, 150 )
