f13970bc9eddab99202404cc85bfae5a317e7948
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-20 20:26:12 -0400

    Fix XP entirely

--- gamemode/sv/PlayerXP.lua
+++ gamemode/sv/PlayerXP.lua
@@ -15,12 +15,35 @@ function dbInternal:CheckTableAndAdd(player)
 	sql.Begin()
 	sql.Query("CREATE TABLE IF NOT EXISTS players_xp (steamid64 VARCHAR(255), xp INTEGER, level INTEGER)")
 	sql.Query("IF NOT EXISTS (SELECT steamid64 FROM players_xp WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "') INSERT INTO players_xp (steamid64, xp, level) VALUES ('" .. player:SteamID64() .. "', 0, 0)")
+
+	local p_xp = sql.QueryValue("SELECT xp FROM players_xp WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
 	sql.Commit()
+
+	player:SetNWInt("XP", tonumber(p_xp))
 end
 
 // modify the player XP
 function dbInternal:ModifyPlayerXP(player, amount)
 	sql.Begin()
-	sql.Query("UPDATE players_xp SET xp = xp + " .. amount .. " WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
+	sql.Query("UPDATE players_xp SET xp = " .. player:GetNWInt("XP") + amount .. " WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
 	sql.Commit()
+	print("[XP] " .. player:Nick() .. " has gained " .. amount .. " XP!")
+	player:SetNWInt("XP", player:GetNWInt("XP") + amount)
+	print("[XP] " .. player:Nick() .. " has " .. player:GetNWInt("XP") .. " XP!")
+end
+
+// update the player XP
+function dbInternal:UpdateXP(player)
+	sql.Begin()
+	sql.Query("UPDATE players_xp SET xp = " .. player:GetNWInt("XP") .. " WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
+	sql.Commit()
+end
+
+function dbInternal:QueryXP(player)
+	sql.Begin()
+	local result = sql.QueryValue("SELECT xp FROM players_xp WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
+
+	sql.Commit()
+
+	return tonumber(result) or result
 end
