d4dca9f4665ad0dd9511351e1973514bc91beaa6
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-21 16:27:40 -0400

    Use PData instead of SQL, works better

--- gamemode/sv/PlayerXP.lua
+++ gamemode/sv/PlayerXP.lua
@@ -11,39 +11,31 @@ function dbInternal:Initialize()
 end
 
 function dbInternal:CheckTableAndAdd(player)
-	/* add player_xp table if it doesn't exist, then add the player to the table */
-	sql.Begin()
-	sql.Query("CREATE TABLE IF NOT EXISTS players_xp (steamid64 VARCHAR(255), xp INTEGER, level INTEGER)")
-	sql.Query("IF NOT EXISTS (SELECT steamid64 FROM players_xp WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "') INSERT INTO players_xp (steamid64, xp, level) VALUES ('" .. player:SteamID64() .. "', 0, 0)")
+	local pd = player:GetPData("XP", -1)
 
-	local p_xp = sql.QueryValue("SELECT xp FROM players_xp WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
-	sql.Commit()
+	if pd == -1 then
+		player:SetPData("XP", 0)
 
-	player:SetNWInt("XP", tonumber(p_xp))
+		pd = 0
+	end
+
+	local pc = player:GetPData("Class", "assault")
+
+	player:SetNWString("WeaponClass", pc)
+	player:SetNWInt("XP", pd)
 end
 
 // modify the player XP
 function dbInternal:ModifyPlayerXP(player, amount)
-	sql.Begin()
-	sql.Query("UPDATE players_xp SET xp = " .. player:GetNWInt("XP") + amount .. " WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
-	sql.Commit()
-	print("[XP] " .. player:Nick() .. " has gained " .. amount .. " XP!")
 	player:SetNWInt("XP", player:GetNWInt("XP") + amount)
-	print("[XP] " .. player:Nick() .. " has " .. player:GetNWInt("XP") .. " XP!")
+	player:SetPData("XP", player:GetNWInt("XP"))
 end
 
-// update the player XP
-function dbInternal:UpdateXP(player)
-	sql.Begin()
-	sql.Query("UPDATE players_xp SET xp = " .. player:GetNWInt("XP") .. " WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
-	sql.Commit()
+function dbInternal:ModifyPlayerClass(player, newClass)
+	player:SetNWString("WeaponClass", newClass)
+	player:SetPData("Class", newClass)
 end
 
 function dbInternal:QueryXP(player)
-	sql.Begin()
-	local result = sql.QueryValue("SELECT xp FROM players_xp WHERE steamid64 = '" .. sql.SQLStr(player:SteamID64()) .. "'")
-
-	sql.Commit()
-
-	return tonumber(result) or result
+	return player:GetNWInt("XP")
 end
