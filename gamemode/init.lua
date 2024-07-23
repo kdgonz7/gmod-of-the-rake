5d4123ff3ecf4e58ce2aa8da346431e0b529262f
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-23 12:49:17 -0400

    (debug) add prints for each file

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,22 +3,23 @@ AddCSLuaFile("shared.lua")
 
 include "shared.lua"
 
-// function GM:SpawnMenuCreated() return false end
-
-
 for _, v in pairs(file.Find("gamemodes/rake/gamemode/sv/*.lua", "GAME")) do
+	print("[Rake Loader] Serverside file loaded: " .. v)
 	include("sv/" .. v)
 end
 
 for _, v in pairs(file.Find("gamemodes/rake/gamemode/shared/*.lua", "GAME")) do
 	AddCSLuaFile("shared/" .. v)
 	include("shared/" .. v)
+
+	print("[Rake Loader] Shared file loaded: " .. v)
 end
 
 for _, v in pairs(file.Find("gamemodes/rake/gamemode/cl/*.lua", "GAME")) do
 	AddCSLuaFile("cl/" .. v)
+
+	print("[Rake Loader] Clientside file loaded: " .. v)
 end
 
-function GM:Initialize()
-
-end
+-- this is the only function you HAVE to define
+function GM:Initialize() end
