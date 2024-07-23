a9b798a9c72c451ab5d1eebe72a65a57b5b0fab2
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-23 18:56:01 -0400

    Add mroe checks

--- gamemode/shared/Extra.lua
+++ gamemode/shared/Extra.lua
@@ -1,4 +1,7 @@
 -- ability to drop weapons
 concommand.Add("drop", function(ply, cmd, args, str)
+	if ! ply then return end
+	if ! ply:GetActiveWeapon() then return end
+
 	ply:DropWeapon(ply:GetActiveWeapon(), nil, Vector(0, 5, 1))
 end)
