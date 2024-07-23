6f9d4bb4a92d0ea104ff4988e8aef8d536877e47
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-23 01:36:15 -0400

    Add comments for more files

--- gamemode/sv/BuyClass.lua
+++ gamemode/sv/BuyClass.lua
@@ -1,26 +1,29 @@
 util.AddNetworkString("rake_BuyClass")
 
+-- simple code to buy a class
 net.Receive("rake_BuyClass", function(len, ply)
-	if roundManage:GetRoundStatus() != IN_LOBBY then return end
-	if not ply:Alive() then return end
+	if roundManage:GetRoundStatus() != IN_LOBBY then return end	-- checks
+	if not ply:Alive() then return end													-- checks
 
-	local className = sql.SQLStr(net.ReadString(), true)
+	-- make sure even i didn't make a mistake
+	local className = sql.SQLStr(net.ReadString(), true)	-- the classname
+	local myXP = tonumber(ply:GetNWInt("XP"))							-- the player's XP
+	local requested_class =	string.lower(className)				-- the class they're requesting
 
-	local myXP = tonumber(ply:GetNWInt("XP"))
-
-	local requested_class =	string.lower(className)
-
-	if dataBase:PlayerHasWeaponClass(ply, requested_class) then
-		dataBase:ModifyPlayerClass(ply, requested_class)
-		print("[Rake] Changed Class to " .. requested_class)
-		return
+	if dataBase:PlayerHasWeaponClass(ply, requested_class) then	-- if they already have that class
+		dataBase:ModifyPlayerClass(ply, requested_class)					-- change their class
+		print("[Rake] Changed Class to " .. requested_class)			-- tell them it changed
+		return																										-- return
 	end
 
+	-- idk why it'd be nil but 
+	-- make sure we have a valid class
 	if requested_class == nil then
 		print("[Rake] no class specified!")
 		return
 	end
 
+	-- list classes if it's called as rake_BuyClass list
 	if (requested_class == "list") then
 		print("A list of available classes to choose from: ")
 		for class, req in pairs(roundManage.XPRequirements) do
@@ -29,6 +32,7 @@ net.Receive("rake_BuyClass", function(len, ply)
 		return
 	end
 
+	-- make sure they have enough XP
 	local req_class_requirement = roundManage.XPRequirements[requested_class]
 
 	if myXP < req_class_requirement then
@@ -36,6 +40,7 @@ net.Receive("rake_BuyClass", function(len, ply)
 		return
 	end
 
+	-- buy the class
 	dataBase:ModifyPlayerXP(ply, -req_class_requirement)
 	dataBase:ModifyPlayerClass(ply, requested_class)
 	dataBase:AddToPlayerInventory(ply, requested_class)
