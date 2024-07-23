a63db7e279dad7f99806cc054159c6502fee6b2d
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-23 12:35:12 -0400

    Remove unused file

--- gamemode/cl/gameStateManager.lua
+++
@@ -1,7 +0,0 @@
-local gameState = CreateClientConVar("rake_GameState", "lobby", FCVAR_NONE, "The state of the game. lobby, match, etc.")
-
-function GetGameState()
-	return gameState:GetString()
-end
-
-print(GetGameState())
