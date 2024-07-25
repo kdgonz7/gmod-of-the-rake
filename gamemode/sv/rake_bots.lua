8b663bf463da2aed2592a2e3472d1ad2fcc9553f
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-24 20:43:14 -0400

    Add new Bot AI

--- gamemode/sv/rake_bots.lua
+++ gamemode/sv/rake_bots.lua
@@ -1,5 +1,7 @@
 util.AddNetworkString("rake_BotSwitchState")
 
+local allnav = navmesh.GetAllNavAreas()
+
 local RandomName = function() local bn = {
 	"RakeKiller07",
 	"Micahnator3000",
@@ -148,6 +150,21 @@ net.Receive("rake_BotSwitchState", function(len, ply_sender)
 	botPlayerEnt:SetNWString("STATE", newState)
 end)
 
+function CanSeeRake(fromPlayer)
+	local enemy = ents.FindByClass("drg_sf2_therake")
+
+	if IsValid(enemy[1]) then
+		local tr = util.QuickTrace(fromPlayer:GetPos(), enemy[1]:GetPos() - fromPlayer:GetPos(), fromPlayer)
+		if tr.Entity == enemy[1] then
+			return true
+		else
+			return false
+		end
+	end
+end
+
+local stat = "FOR-PLAYER"
+
 hook.Add("StartCommand", "RakeStartCommand", function(ply, cmd)
 	if ! ply:IsBot() then return end
 
@@ -176,9 +193,17 @@ hook.Add("StartCommand", "RakeStartCommand", function(ply, cmd)
 		ply.lastRePath = CurTime()
 	end
 
+	local enemy = ents.FindByClass("drg_sf2_therake")
+
 	-- if there's no path, and our delay is past we'll get a new path
 	if ( !ply.path && ply.lastRePath2 + rePathDelay < CurTime() ) then
 		local aPlayer = player.GetHumans()[ math.random( 1, #player.GetHumans() ) ]
+		stat = "FOR-PLAYER"
+		if stat == "FIND-RAKE" then
+			if IsValid(enemy[1]) then
+				aPlayer = enemy[1]
+			end
+		end
 
 		-- the target position to go to
 		local targetPos = aPlayer:GetPos()
@@ -200,48 +225,79 @@ hook.Add("StartCommand", "RakeStartCommand", function(ply, cmd)
 		table.remove( ply.path ) -- Just for this example, remove the starting area, we are already in it!
 	end
 
+	if not ply.path and ply.lastRePath2 + rePathDelay < CurTime() then
+		stat = "FIND-RAKE"
+	end
 
-	-- we have no path, or its empty (we arrived at the goal), try to get a new path.
-	if ( !ply.path ) then
-		ply.path = nil
-		ply.targetArea = nil
-		-- patrol
 
-		if ( math.random( 1, 100 ) > 50 ) then
-			local allnav = navmesh.GetAllNavAreas()
-			local random = math.random( 1, #allnav )
+	if IsValid(enemy[1]) then
+		-- we have no path, or its empty (we arrived at the goal), try to get a new path.
+		if not ply.path and ply.lastRePath2 + rePathDelay < CurTime() then
+			local enemy = ents.FindByClass("drg_sf2_therake")[1]
 
-			ply.path = Pathfind_AS( currentArea, allnav[ random ] )
-			ply.targetArea = allnav[random]
-		end
-		return
+			if IsValid(enemy) then
+					local targetPos = enemy:GetPos()
+					local targetArea = navmesh.GetNearestNavArea(targetPos)
+
+					ply.path = Pathfind_AS(currentArea, targetArea)
+					if istable(ply.path) then
+							table.remove(ply.path)
+					else
+							ply.path = nil
+							ply.lastRePath2 = CurTime()
+							return
+					end
+
+					ply.targetArea = nil
+			end
+
+			ply.lastRePath2 = CurTime()
+	end
 	end
 
 	-- Select the next area we want to go into
 	if ( !IsValid( ply.targetArea ) ) then
+		if ( !istable( ply.path ) ) then
+			ply.path = nil
+			return
+		end
 		ply.targetArea = ply.path[ #ply.path ]
 	end
 
-	-- The area we selected is invalid or we are already there, remove it, bail and wait for next cycle
+	-- The area we selected is invalid or we are already there, then we need to get a new path
 	if ( !IsValid( ply.targetArea ) || ( ply.targetArea == currentArea && ply.targetArea:GetCenter():Distance( ply:GetPos() ) < 64 ) ) then
-		table.remove( ply.path ) -- Removes last element
+		if ( istable( ply.path ) ) then
+			table.remove( ply.path ) -- Removes last element
+		else
+			ply.path = nil
+		end
+
 		ply.targetArea = nil
+
 		return
 	end
 
-	local enemy = ents.FindByClass("drg_sf2_therake")
 
 	local targetAngle = ( ply.targetArea:GetCenter() - ply:GetPos() ):GetNormalized():Angle()
 
-	if IsValid(enemy[1]) && enemy[1]:GetPos():Distance(ply:GetPos()) < 300 then
-			local targetAngle = (enemy[1]:GetPos() - ply:GetPos()):GetNormalized():Angle()
+	if IsValid(enemy[1]) && enemy[1]:GetPos():Distance(ply:GetPos()) < 400 then
+			local targetAngle = ((enemy[1]:GetPos() + Vector(0, 0, enemy[1]:OBBCenter()))  - ply:GetPos()):GetNormalized():Angle()
 			cmd:SetViewAngles(targetAngle)
-			cmd:SetButtons(IN_ATTACK)
 			ply:SetEyeAngles(targetAngle)
+			cmd:SetButtons(IN_ATTACK)
+			cmd:AddKey(IN_DUCK)
 
 			if ply:GetActiveWeapon():Clip1() == 0 then
 				ply:GiveAmmo(1000, ply:GetActiveWeapon():GetPrimaryAmmoType())
-				cmd:AddKey(IN_RELOAD)
+
+				-- we only want to reload in a safe area.
+				if !CanSeeRake(ply) then
+					cmd:AddKey(IN_RELOAD)
+				else
+					ply.path = Pathfind_AS( currentArea, ply.targetArea )
+					cmd:SetForwardMove(1000)
+					cmd:AddKey(IN_SPEED)
+				end
 			end
 
 			return -- Ensure that shooting takes priority over navigation
@@ -252,4 +308,17 @@ hook.Add("StartCommand", "RakeStartCommand", function(ply, cmd)
 	cmd:SetViewAngles(targetAngle)
 	ply:SetEyeAngles(targetAngle)
 	cmd:SetForwardMove(1000)
+
+	local mightNeedToCrouchTrace = util.QuickTrace(ply:GetPos() + Vector(0, 0, 5), targetAngle:Forward() * 0.6, ply)
+	if ( mightNeedToCrouchTrace.Hit ) then
+		cmd:SetButtons(IN_DUCK)
+	end
+
+	local doorTrace = ply:GetEyeTraceNoCursor()
+	-- if doorTrace.Hit then
+	-- if ( doorTrace.Hit && doorTrace.Entity:GetClass() == "prop_door_rotating" && doorTrace.Entity:GetPos():Distance(ply:GetPos()) < 100) then
+	-- 	cmd:SetButtons(IN_USE)
+	-- 	return
+	-- end
+-- end
 end)
